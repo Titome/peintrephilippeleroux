aad76102d2c19776ca847f0d9c52694685cb518a
Titome <girard.timothee@gmail.com>
2012-05-14 20:09:46 +0200

    rajout du bundle Avalanche + librairie Imagine. Bundle fonctionnel

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -17,6 +17,7 @@ class AppKernel extends Kernel
             new Symfony\Bundle\AsseticBundle\AsseticBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new JMS\SecurityExtraBundle\JMSSecurityExtraBundle(),
+            new Avalanche\Bundle\ImagineBundle\AvalancheImagineBundle(),
             new Titome\PeintrePhilippeLerouxBundle\TitomePeintrePhilippeLerouxBundle(),
         );
 
