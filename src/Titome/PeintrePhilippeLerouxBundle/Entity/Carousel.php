909778aba5300bfc63d947aecc710eca1f8a5b59
Titome <girard.timothee@gmail.com>
2012-06-01 21:01:52 +0200

    création de l'entité carousel

---
+++ src/Titome/PeintrePhilippeLerouxBundle/Entity/Carousel.php
@@ -0,0 +1,115 @@
+<?php
+
+namespace Titome\PeintrePhilippeLerouxBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Titome\PeintrePhilippeLerouxBundle\Entity\Carousel
+ *
+ * @ORM\Table()
+ * @ORM\Entity(repositoryClass="Titome\PeintrePhilippeLerouxBundle\Entity\CarouselRepository")
+ */
+class Carousel
+{
+    /**
+     * @var integer $id
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string $image
+     *
+     * @ORM\Column(name="image", type="string", length=255)
+     */
+    private $image;
+
+    /**
+     * @var string $titre
+     *
+     * @ORM\Column(name="titre", type="string", length=255)
+     */
+    private $titre;
+
+    /**
+     * @var text $legende
+     *
+     * @ORM\Column(name="legende", type="text")
+     */
+    private $legende;
+
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set image
+     *
+     * @param string $image
+     */
+    public function setImage($image)
+    {
+        $this->image = $image;
+    }
+
+    /**
+     * Get image
+     *
+     * @return string 
+     */
+    public function getImage()
+    {
+        return $this->image;
+    }
+
+    /**
+     * Set titre
+     *
+     * @param string $titre
+     */
+    public function setTitre($titre)
+    {
+        $this->titre = $titre;
+    }
+
+    /**
+     * Get titre
+     *
+     * @return string 
+     */
+    public function getTitre()
+    {
+        return $this->titre;
+    }
+
+    /**
+     * Set legende
+     *
+     * @param text $legende
+     */
+    public function setLegende($legende)
+    {
+        $this->legende = $legende;
+    }
+
+    /**
+     * Get legende
+     *
+     * @return text 
+     */
+    public function getLegende()
+    {
+        return $this->legende;
+    }
+}
