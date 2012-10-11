2e3f38a4e16ee58070e1efb2b7b8f5baadba6393
Titome <girard.timothee@gmail.com>
2012-10-11 10:58:37 +0200

    test email

---
+++ src/Titome/PeintrePhilippeLerouxBundle/Controller/ContactController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Titome\PeintrePhilippeLerouxBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class ContactController extends Controller
+{
+	public function indexAction()
+	{
+		$message = \Swift_Message::newInstance()
+			->setSubject('Hello Email')
+			->setFrom('timothee@timotheegirard.com')
+			->setTo('girard.timothee@gmail.com')
+			->setBody('Test')
+		;
+		$this->get('mailer')->send($message);
+
+		$this->get('session')->setFlash('notice', 'Email envoyé !');
+		return $this->render('TitomePeintrePhilippeLerouxBundle:Contact:contact.html.twig');
+	}
+}
