20ccd5db00a2d1fdabc611c7010d38665351c276
Titome <girard.timothee@gmail.com>
2012-10-12 10:14:29 +0200

    changement adresse mail

--- web/app_dev.php
+++ web/app_dev.php
@@ -13,6 +13,7 @@
         || !in_array(@$_SERVER['REMOTE_ADDR'], array(
             '127.0.0.1',
             '::1',
+	    '109.190.56.31',
         ))
     ) {
         header('HTTP/1.0 403 Forbidden');
